012a2334559cd353bf82241c94118a5860c5eba4
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-03 11:26:11 -0400

    - #13: Minor change to adapt to Storybook for StoryName and function name generated UI

--- src/components/BasicCard.stories.tsx
+++ src/components/BasicCard.stories.tsx
@@ -2,15 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { fn } from 'storybook/test';
 
-import { BasicCard } from './BasicCard';
+import { BasicCard, type BasicCardProps } from './BasicCard';
 
-export const ActionData = {
-  assetImage: 'tech-attack-training-1.png',
+const defaultProps: BasicCardProps = {
+  assetImage: 'hanzhong/tech/attack-training-1.png',
   label: 'Nice Card',
   value: 5,
   maxValue: 10,
-  onIncrease: fn(),
-  onDecrease: fn(),
+  onChange: fn(),
 };
 
 const meta = {
@@ -18,7 +17,7 @@ const meta = {
   component: BasicCard,
   tags: ['autodocs'],
   args: {
-    ...ActionData,
+    ...defaultProps,
   },
 } satisfies Meta<typeof BasicCard>;
 
@@ -26,32 +25,41 @@ type Story = StoryObj<typeof meta>;
 
 export default meta;
 
+export const Default: Story = {
+  args: {
+    ...defaultProps,
+  },
+};
+Default.storyName = 'happy case';
+
 export const ValueZero: Story = {
   args: {
-    assetImage: 'tech-attack-training-1.png',
-    label: 'Nice Card',
+    ...defaultProps,
     value: 0,
-    maxValue: 10,
   },
 };
 ValueZero.storyName = 'value = 0';
 
+export const MaxValue: Story = {
+  args: {
+    ...defaultProps,
+    value: 10,
+  },
+};
+MaxValue.storyName = 'value = maxValue';
+
 export const NegativeValue: Story = {
   args: {
-    assetImage: 'tech-attack-training-1.png',
-    label: 'Nice Card',
+    ...defaultProps,
     value: -1,
-    maxValue: 10,
   },
 };
 NegativeValue.storyName = 'value < 0';
 
 export const ValueAboveMaxValue: Story = {
   args: {
-    assetImage: 'tech-attack-training-1.png',
-    label: 'Nice Card',
+    ...defaultProps,
     value: 11,
-    maxValue: 10,
   },
 };
 ValueAboveMaxValue.storyName = 'value > maxValue';
--- src/components/BasicCard.tsx
+++ src/components/BasicCard.tsx
@@ -2,21 +2,26 @@ import ArrowDownwardSharpIcon from '@mui/icons-material/ArrowDownwardSharp';
 import ArrowUpwardSharpIcon from '@mui/icons-material/ArrowUpwardSharp';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+
 import { BasicCardImage } from './BasicCardImage';
-import Grid from '@mui/material/Grid';
 
 export interface BasicCardProps {
   assetImage: string;
   label: string;
   value: number;
   maxValue: number;
-  onIncrease: () => void;
-  onDecrease: () => void;
+  onChange: (newValue: number) => void;
 }
 
-export const BasicCard = ({ assetImage, label, value, maxValue, onDecrease, onIncrease }: BasicCardProps) => {
+export const BasicCard = ({ assetImage, label, value, maxValue, onChange }: BasicCardProps) => {
+  const onIncrease = () => onChange(value + 1);
+  const onDecrease = () => onChange(value - 1);
+
+  const hasError = value < 0 || value > maxValue;
+
   return (
     <Card sx={{ width: 200 }}>
       <CardContent>
@@ -25,13 +30,13 @@ export const BasicCard = ({ assetImage, label, value, maxValue, onDecrease, onIn
             <BasicCardImage assetImage={assetImage} />
           </Grid>
           <Grid size={{ xs: 6 }}>
-            <IconButton title="Increase value" onClick={onIncrease} disabled={value >= maxValue}>
+            <IconButton title="Increase value" onClick={onIncrease} disabled={hasError || value === maxValue}>
               <ArrowUpwardSharpIcon />
             </IconButton>
-            <Typography align="center" color={value < 0 || value > maxValue ? 'error' : undefined}>
+            <Typography align="center" color={hasError ? 'error' : undefined}>
               {value} / {maxValue}
             </Typography>
-            <IconButton title="Decrease value" onClick={onDecrease} disabled={value <= 0}>
+            <IconButton title="Decrease value" onClick={onDecrease} disabled={hasError || value === 0}>
               <ArrowDownwardSharpIcon />
             </IconButton>
           </Grid>
