5f8ee72b46157f5f6aa244de523fac2b06e0698d
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-03 17:55:54 -0400

    - #11: Adding Hanzhong Tech component.

---
+++ src/components/hanzhong/HanzhongTech.tsx
@@ -0,0 +1,22 @@
+import type { HanzhongTechType } from '../../types/hanzhong-tech-type';
+import { BasicCard } from '../BasicCard';
+
+export interface HanzhongTechProps {
+  info: HanzhongTechType;
+  value: number;
+  onChange: (id: string, newValue: number) => void;
+}
+
+export const HanzhongTech = ({ info, value, onChange }: HanzhongTechProps) => {
+  const onChangeValue = (newValue: number) => onChange(info.id, newValue);
+
+  return (
+    <BasicCard
+      assetImage={info.assetPath}
+      label={info.label}
+      value={value}
+      maxValue={info.levels.length}
+      onChange={onChangeValue}
+    />
+  );
+};
--- src/types/hanzhong-tech-type.ts
+++ src/types/hanzhong-tech-type.ts
@@ -2,6 +2,7 @@ import type { BaseIDType } from './base-id-type';
 import type { HanzhongTechLevelType } from './hanzhong-tech-level-type';
 
 export type HanzhongTechType = BaseIDType & {
+  assetPath: string;
   label: string;
   description: string;
   levels: HanzhongTechLevelType[];
