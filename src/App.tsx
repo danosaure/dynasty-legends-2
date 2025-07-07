ea3aec10072cd2a5cd4823a9d1fda11f082e26a4
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-06 23:43:33 -0400

    - Modified to an empty shell.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Typography variant="h1">Vite and React</Typography>
-      <div className="card">
-        <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Grid container direction="row" spacing={2} sx={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+      <Grid container>
+        <Grid size={{ xs: 3 }}>LOGO</Grid>
+        <Grid size={{ xs: 9 }}>TOP</Grid>
+        <Grid size={{ xs: 3 }}>Left menu</Grid>
+        <Grid size={{ xs: 9 }}>
+          <Paper elevation={3}>Main content</Paper>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
-
-export default App;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { App } from './App.tsx';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
