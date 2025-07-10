77c04bca4b0b14743ccf25675ebb69d95c60f5c9
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-10 14:55:36 -0400

    - #7: Adding simple Tabs navigation.

--- src/hanzhong/components/Hanzhong.tsx
+++ src/hanzhong/components/Hanzhong.tsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react';
 
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 import { HanzhongContext, type HanzhongContextType } from './HanzhongContext';
-import { HanzhongSidePanel } from './HanzhongSidePanel';
 import { HanzhongWarTiersTechs } from './HanzhongWarTiersTechs';
-import { HANZHONG_DATA } from '../data';
+import { OccupiedCities } from './OccupiedCities';
+import { OccupiedTerritories } from './OccupiedTerritories';
+import { ResourceIncomes } from './ResourceIncomes';
+import { TacticalBonuses } from './TacticalBonuses';
 import { initializeEarnings } from './utils/initialize-earnings';
+import { HANZHONG_DATA } from '../data';
 import type { HanzhongBonusType } from '../types';
+import { SectionTabpanel } from '../../components/SectionTabpanel';
+import { generateTabA11yProps } from '../../components/utils/generate-tab-a11y-props';
 import type { HanzhongUserDataType } from '../../persistence/hanzhong-user-data-type';
 
 export const Hanzhong = () => {
   const [loading, setLoading] = useState<boolean>(true);
-
   const [userData, setUserData] = useState<HanzhongUserDataType>({});
+  const [selectedTabName, setSelectedTabName] = useState<string>('techs');
 
   const [hanzhongContextData, setHanzhongContextData] = useState<HanzhongContextType>({
     hanzhong: HANZHONG_DATA,
@@ -47,17 +55,46 @@ export const Hanzhong = () => {
     return null;
   }
 
+  const changeTab = (e, value: string): void => setSelectedTabName(value);
+
   return (
     <HanzhongContext.Provider value={hanzhongContextData}>
       <Grid container spacing={2}>
         <Grid size={{ xs: 3 }}>
-          <Paper elevation={10} sx={{ p: 2 }}>
-            <HanzhongSidePanel />
-          </Paper>
+          <Grid container spacing={3}>
+            <Box sx={{ width: '100%' }}>
+              <Tabs value={selectedTabName} onChange={changeTab} orientation="vertical" sx={{ borderRight: 1 }}>
+                {[
+                  ['Techs', 'techs'],
+                  ['Territories', 'territories'],
+                  ['Siege Progress', 'cities'],
+                  ['War Tiers', 'warTiers'],
+                ].map(([label, id]) => (
+                  <Tab label={label} value={id} {...generateTabA11yProps('hanzhong', id)} sx={{ alignItems: 'end' }} />
+                ))}
+              </Tabs>
+            </Box>
+
+            <ResourceIncomes />
+            <OccupiedTerritories />
+            <OccupiedCities />
+            <TacticalBonuses />
+          </Grid>
         </Grid>
         <Grid size={{ xs: 9 }}>
           <Paper elevation={10} sx={{ p: 2 }}>
-            <HanzhongWarTiersTechs />
+            <SectionTabpanel selectedTabName={selectedTabName} tabsName="hanzhong" tabName="techs">
+              <HanzhongWarTiersTechs />
+            </SectionTabpanel>
+            <SectionTabpanel selectedTabName={selectedTabName} tabsName="hanzhong" tabName="territories">
+              Territories UI
+            </SectionTabpanel>
+            <SectionTabpanel selectedTabName={selectedTabName} tabsName="hanzhong" tabName="cities">
+              Cities UI
+            </SectionTabpanel>
+            <SectionTabpanel selectedTabName={selectedTabName} tabsName="hanzhong" tabName="warTiers">
+              War Tiers UI
+            </SectionTabpanel>
           </Paper>
         </Grid>
       </Grid>
