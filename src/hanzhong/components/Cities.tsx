3545c762c705a473bcfa271826b780de2f42e89f
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-11 13:23:31 -0400

    - #031: Also modifying the view for Cities.

--- src/hanzhong/components/Cities.tsx
+++ src/hanzhong/components/Cities.tsx
@@ -1,17 +1,56 @@
 import Grid from '@mui/material/Grid';
 
-import { useHanzhongContext } from './HanzhongContext';
-import type { HanzhongCityType } from '../types';
 import { City } from './City';
+import { useHanzhongContext } from './HanzhongContext';
+import { HANZHONG_CITY_IDS } from '../constants/items-ids';
+import { findCityById } from '../utils/find-city-by-id';
 
 export const Cities = () => {
   const { hanzhong } = useHanzhongContext();
 
+  const topNorthCity = findCityById(hanzhong, HANZHONG_CITY_IDS.NORTH_SUMMIT);
+  const northCity = findCityById(hanzhong, HANZHONG_CITY_IDS.MOUNT_DINGJUN);
+  const westCity = findCityById(hanzhong, HANZHONG_CITY_IDS.YANGPING_PASS);
+  const centerCity = findCityById(hanzhong, HANZHONG_CITY_IDS.HANZHONG_CITY);
+  const eastCity = findCityById(hanzhong, HANZHONG_CITY_IDS.MAMING_PAVILION);
+  const southCity = findCityById(hanzhong, HANZHONG_CITY_IDS.HANSHUI_TRAIL);
+  const bottomSouthCity = findCityById(hanzhong, HANZHONG_CITY_IDS.MIANYANG_COUNTY);
+
   return (
     <Grid container size={{ xs: 12 }} spacing={2}>
-      {hanzhong.cities.map((city: HanzhongCityType) => (
-        <City key={city.id} city={city} />
-      ))}
+      <Grid size={{ xs: 4 }} />
+      <Grid size={{ xs: 4 }}>
+        <City city={topNorthCity} />
+      </Grid>
+      <Grid size={{ xs: 4 }} />
+
+      <Grid size={{ xs: 4 }} />
+      <Grid size={{ xs: 4 }}>
+        <City city={northCity} />
+      </Grid>
+      <Grid size={{ xs: 4 }} />
+
+      <Grid size={{ xs: 4 }}>
+        <City city={westCity} />
+      </Grid>
+      <Grid size={{ xs: 4 }}>
+        <City city={centerCity} />
+      </Grid>
+      <Grid size={{ xs: 4 }}>
+        <City city={eastCity} />
+      </Grid>
+
+      <Grid size={{ xs: 4 }} />
+      <Grid size={{ xs: 4 }}>
+        <City city={southCity} />
+      </Grid>
+      <Grid size={{ xs: 4 }} />
+
+      <Grid size={{ xs: 4 }} />
+      <Grid size={{ xs: 4 }}>
+        <City city={bottomSouthCity} />
+      </Grid>
+      <Grid size={{ xs: 4 }} />
     </Grid>
   );
 };
--- src/hanzhong/components/City.tsx
+++ src/hanzhong/components/City.tsx
@@ -1,11 +1,11 @@
-import Grid from '@mui/material/Grid';
-
-import type { HanzhongCityType } from '../types';
-import Checkbox from '@mui/material/Checkbox';
 import { useState, type ChangeEvent } from 'react';
-import { useHanzhongContext } from './HanzhongContext';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+
+import { useHanzhongContext } from './HanzhongContext';
+import type { HanzhongCityType } from '../types';
 
 export type CityProps = {
   city: HanzhongCityType;
@@ -22,11 +22,13 @@ export const City = ({ city }: CityProps) => {
   };
 
   return (
-    <Card sx={{ width: 200 }} elevation={10}>
-      <CardContent sx={{ 'p': 1, '&:last-child': { pb: 1 } }}>
-        <Grid container size={{ xs: 12 }} sx={{ alignItems: 'center', justifyContent: 'center' }} spacing={2}>
-          <Grid size={{ xs: 8 }}>{city.name}</Grid>
-          <Grid size={{ xs: 4 }}>
+    <Card elevation={10}>
+      <CardContent sx={{ 'p': 2, '&:last-child': { pb: 2 } }}>
+        <Grid container size={{ xs: 12 }} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Grid size={{ xs: 12 }} sx={{ textAlign: 'center' }}>
+            {city.name}
+          </Grid>
+          <Grid size={{ xs: 12 }} sx={{ textAlign: 'center' }}>
             <Checkbox checked={checked} onChange={checkboxChanged} />
           </Grid>
         </Grid>
