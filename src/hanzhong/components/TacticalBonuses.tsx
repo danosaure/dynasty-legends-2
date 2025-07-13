94054c93c9ce54bf7d5df13315668929d975f549
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-12 20:27:51 -0400

    - #36: Removed unused code.

--- src/hanzhong/components/TacticalBonuses.tsx
+++ src/hanzhong/components/TacticalBonuses.tsx
@@ -2,31 +2,11 @@ import { HanzhongInfosDisplay } from './HanzhongInfosDisplay';
 import { useHanzhongContext } from './HanzhongContext';
 import type { HanzhongInfoDisplayProps } from './HanzhongInfoDisplay';
 import { calculateTacticalPoints } from '../utils/calculate-tactical-points';
-import { TACTICAL_POINTS_PER_ATK, TACTICAL_POINTS_PER_DEF, TACTICAL_POINTS_PER_HP } from '../constants/tactical-points-per-bonus';
 
 export const TacticalBonuses = () => {
-  const { bonuses, bonusesVanguardCamp, bonusesValiantCavalry, bonusesRoyalGuards } = useHanzhongContext();
+  const { bonusesVanguardCamp, bonusesValiantCavalry, bonusesRoyalGuards } = useHanzhongContext();
 
   const items: HanzhongInfoDisplayProps[] = [
-    // {
-    //   label: 'ATK',
-    //   value: bonuses.atk ?? 0,
-    //   unit: `x${TACTICAL_POINTS_PER_ATK}`,
-    // },
-    // {
-    //   label: 'DEF',
-    //   value: bonuses.def ?? 0,
-    //   unit: `x${TACTICAL_POINTS_PER_DEF}`,
-    // },
-    // {
-    //   label: 'HP',
-    //   value: bonuses.hp ?? 0,
-    //   unit: `x${TACTICAL_POINTS_PER_HP}`,
-    // },
-    // {
-    //   label: 'Tactical Points',
-    //   value: calculateTacticalPoints(bonuses),
-    // },
     {
       label: 'Vanguard Camp',
       value: calculateTacticalPoints(bonusesVanguardCamp),
