49165af9aba4033aa24e8b018069a561002a7e39
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-17 17:45:11 -0400

    - Responsive Cities page.

--- src/hanzhong/components/cities.tsx
+++ src/hanzhong/components/cities.tsx
@@ -18,43 +18,50 @@ export const HanzhongCities = () => {
   const southCity = findCityById(hanzhong, HANZHONG_CITY_IDS.HANSHUI_TRAIL);
   const bottomSouthCity = findCityById(hanzhong, HANZHONG_CITY_IDS.MIANYANG_COUNTY);
 
+  const lastCell = <Grid size={{ md: 3, lg: 6 }} sx={{ display: { xs: 'none', md: 'block' } }} />;
+
   return (
     <>
       <TitleTypographyWrapper variant="TITLE">Cities</TitleTypographyWrapper>
       <Grid container size={{ xs: 12 }} spacing={2}>
-        <Grid size={{ xs: 4 }} />
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={topNorthCity} />
         </Grid>
-        <Grid size={{ xs: 4 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        {lastCell}
 
-        <Grid size={{ xs: 4 }} />
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={northCity} />
         </Grid>
-        <Grid size={{ xs: 4 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        {lastCell}
 
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={westCity} />
         </Grid>
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={centerCity} />
         </Grid>
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={eastCity} />
         </Grid>
+        {lastCell}
 
-        <Grid size={{ xs: 4 }} />
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={southCity} />
         </Grid>
-        <Grid size={{ xs: 4 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        {lastCell}
 
-        <Grid size={{ xs: 4 }} />
-        <Grid size={{ xs: 4 }}>
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }}>
           <HanzhongCity city={bottomSouthCity} />
         </Grid>
-        <Grid size={{ xs: 4 }} />
+        <Grid size={{ xs: 4, md: 3, lg: 2 }} />
+        {lastCell}
       </Grid>
     </>
   );
