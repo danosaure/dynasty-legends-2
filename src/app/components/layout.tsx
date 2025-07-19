29ef445698390aceda7bf8da27f962d5772ed969
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-19 00:04:19 -0400

    - #53: Fixing the container max-width so we don't have to think about bigger screen.

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,12 +1,15 @@
 import Grid from '@mui/material/Grid';
 import { MainHeader } from './header';
 import { Outlet } from 'react-router';
+import Container from '@mui/material/Container';
 
 export const MainLayout = () => {
   return (
-    <Grid container size={{ xs: 12 }} direction={'column'}>
-      <MainHeader />
-      <Outlet />
-    </Grid>
+    <Container maxWidth="lg" sx={{ maxWidth: '960px !important', paddingLeft: '4px', paddingRight: '4px' }}>
+      <Grid container size={{ xs: 12 }} direction={'column'}>
+        <MainHeader />
+        <Outlet />
+      </Grid>
+    </Container>
   );
 };
