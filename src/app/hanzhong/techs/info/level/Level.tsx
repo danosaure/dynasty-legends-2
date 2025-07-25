dcdaaa86bdc62ff200056072e954a44f19817359
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-24 20:18:01 -0400

    - #69: WIP start copy code before refactoring for estimated time.

--- src/app/hanzhong/techs/info/level/Level.tsx
+++ src/app/hanzhong/techs/info/level/Level.tsx
@@ -4,6 +4,10 @@ import type { HanzhongTechsTechLevelDisplayType } from '../types';
 
 import { HanzhongTechsTechInfoLevelResources } from './LevelResources';
 import { HanzhongTechsTechInfoLevelBonuses } from './LevelBonuses';
+import { useHanzhongContext } from '../../../HanzhongContext';
+import { extractResourceDataFromUser } from '../../../resources-timers/utils/extract-resource-data-from-user';
+import { calculateTimeNeededForResource } from '../../../resources-timers/utils/calculate-time-needed-for-resource';
+import { TIME_KEYS } from '../../../resources-timers/utils';
 
 export type HanzhongTechsTechInfoLevelProps = {
   info: HanzhongTechsTechLevelDisplayType;
@@ -11,6 +15,24 @@ export type HanzhongTechsTechInfoLevelProps = {
 };
 
 export const HanzhongTechsTechInfoLevel = ({ info, userLevel }: HanzhongTechsTechInfoLevelProps) => {
+  const { bonuses, user } = useHanzhongContext();
+
+  const resourceData = extractResourceDataFromUser(user, bonuses);
+
+  const dataToProcess = [
+    [info.resources.lumber, resourceData.lumber, resourceData.woodRate],
+    [info.resources.grains, resourceData.grains, resourceData.grainsRate],
+    [info.resources.iron, resourceData.iron, resourceData.ironRate],
+  ];
+
+  const [timeForLumber, timeForGrains, timeForIron] = dataToProcess.map(([quantityNeeded, currentInventory, productionRate]) =>
+    calculateTimeNeededForResource(currentInventory, quantityNeeded, productionRate)
+  );
+
+  if (timeForLumber === null || timeForGrains === null || timeForIron === null) {
+    return TIME_KEYS.DONT_KNOW;
+  }
+
   return (
     <Grid container size={12} sx={{ mb: '6px', border: `1px solid `, p: '3px' }} spacing={2}>
       <Grid size={2}>
