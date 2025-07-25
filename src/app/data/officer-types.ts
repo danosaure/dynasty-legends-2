c37b93590295b3e2f9ef827b8cb06f4e5701ca18
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-25 11:01:28 -0400

    - Adding color-coded officer type.

--- src/app/data/officer-types.ts
+++ src/app/data/officer-types.ts
@@ -6,8 +6,33 @@ const RAW: OfficerTypeType[] = [
   {
     id: 'officer-type--mage',
     name: 'Mage',
+    color: '#35587E',
     description:
       'Full of wonderful foresights and good plans. Reduces the backswing duration after Flash (Only the effect of the first type applies in battle for dual-type Officers).',
+  },
+  {
+    id: 'officer-type--marksman',
+    name: 'Marksman',
+    color: '#67337C',
+    description: '',
+  },
+  {
+    id: 'officer-type--support',
+    name: 'Support',
+    color: '#3E7A40',
+    description: '',
+  },
+  {
+    id: 'officer-type--tank',
+    name: 'Tank',
+    color: '#796531',
+    description: '',
+  },
+  {
+    id: 'officer-type--warrior',
+    name: 'Warrior',
+    color: '#7C323D',
+    description: '',
   },
 ];
 
@@ -18,3 +43,8 @@ export const getOfficerTypeById = (id: string): OfficerTypeType | undefined =>
 
 export const getOfficerTypeByName = (name: string): OfficerTypeType | undefined =>
   OFFICER_TYPES.find((officerType) => officerType.name === name);
+
+export const getOfficerTypeIdByName = (name: string): string | undefined => {
+  const officerType = getOfficerTypeByName(name);
+  return officerType?.id;
+};
--- src/app/types/officer-type.d.ts
+++ src/app/types/officer-type.d.ts
@@ -3,4 +3,5 @@ import type { BaseIDType } from './base-id-type';
 export type OfficerTypeType = BaseIDType & {
   name: 'Mage' | 'Marksman' | 'Support' | 'Tank' | 'Warrior';
   description: string;
+  color: string;
 };
