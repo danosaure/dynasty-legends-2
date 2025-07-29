ee062b088301df796e9a9a1b53fed77fab604a60
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-07-29 10:09:49 -0400

    - #71: Moved under territories/ folder.

--- src/app/hanzhong/techs/HanzhongTechsTechCardImage.tsx
+++ src/app/hanzhong/techs/HanzhongTechsTechCardImage.tsx
@@ -1,14 +1,15 @@
+import { assetPath } from '../../utils';
+
 export type HanzhongTechsTechCardImageProps = {
   assetImage: string;
 };
 
 export const HanzhongTechsTechCardImage = ({ assetImage }: HanzhongTechsTechCardImageProps) => {
-  const src = `${import.meta.env.BASE_URL}assets/${assetImage}`;
   const imgStyle = {
     maxWidth: '100%',
     maxHeight: '100%',
     opacity: 0.9,
   };
 
-  return <img src={src} alt={assetImage} style={imgStyle} />;
+  return <img src={assetPath(assetImage)} alt={assetImage} style={imgStyle} />;
 };
