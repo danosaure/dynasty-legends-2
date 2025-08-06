fca2f51561604fc6429d5064fba51eb48825d9f1
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-06 11:09:33 -0400

    - #92: Shuffling code to enable requirement checks for cities.

--- src/app/hanzhong/data/hanzhong-cities.ts
+++ src/app/hanzhong/data/hanzhong-cities.ts
@@ -1,89 +1,142 @@
+import { sanitizeId } from '../../../utils';
 import type { HanzhongCityType } from '../types';
-import { HANZHONG_CITY_IDS } from '../constants/items-ids';
+
+export const cityIdByName = (name: string): string => `hanzhong--city--${sanitizeId(name)}`;
+
+const baseInfo = (name: string) => ({
+  id: cityIdByName(name),
+  name,
+});
+
+export const HANZHONG_CITY_IDS = {
+  YANGPING_PASS: 'hanzhong--city--yangping-pass',
+  NORTH_SUMMIT: 'hanzhong--city--north-summit',
+  MAMING_PAVILION: 'hanzhong--city--maming-pavilion',
+  MIANYANG_COUNTY: 'hanzhong--city--mianyang-county',
+  MOUNT_DINGJUN: 'hanzhong--city--mount-dingjun',
+  HANSHUI_TRAIL: 'hanzhong--city--hanshui-trail',
+  HANZHONG_CITY: 'hanzhong--city--hanzhong-city',
+};
+
+const HANZHONG__CITY__YANGPING_PASS: HanzhongCityType = {
+  position: '(61,106)',
+  tacticalPoints: 2000000,
+  description: 'Increases Wei Officers damage to the garrison by 20%',
+  earnings: {
+    woodRate: 180,
+    grainsRate: 180,
+    ironRate: 180,
+  },
+} as const;
+
+const HANZHONG__CITY__NORTH_SUMMIT: HanzhongCityType = {
+  ...baseInfo('North Summit'),
+  position: '(52,74)',
+  tacticalPoints: 2000000,
+  description: 'Increases Shu Officers damage to the garrison by 20%',
+  earnings: {
+    woodRate: 180,
+    grainsRate: 180,
+    ironRate: 180,
+  },
+} as const;
+
+const HANZHONG__CITY__MAMING_PAVILION: HanzhongCityType = {
+  ...baseInfo('Maming Pavilion'),
+  position: '(90,63)',
+  tacticalPoints: 2000000,
+  description: 'Increases Wu Officers damage to the garrison by 20%',
+  earnings: {
+    woodRate: 180,
+    grainsRate: 180,
+    ironRate: 180,
+  },
+} as const;
+
+const HANZHONG__CITY__MIANYANG_COUNTY: HanzhongCityType = {
+  ...baseInfo('Mianyang County'),
+  position: '(99,96)',
+  tacticalPoints: 2000000,
+  description: 'Increases Other Officers damage to the garrison by 20%',
+  earnings: {
+    woodRate: 180,
+    grainsRate: 180,
+    ironRate: 180,
+  },
+} as const;
+
+const HANZHONG__CITY__MOUNT_DINGJUN: HanzhongCityType = {
+  ...baseInfo('Mount Dingjun'),
+  position: '(63,81)',
+  tacticalPoints: 5000000,
+  description: "Reduces the garrison's damage taken by 20%",
+  earnings: {
+    woodRate: 360,
+    grainsRate: 360,
+    ironRate: 360,
+  },
+} as const;
+
+const HANZHONG__CITY__HANSHUI_TRAIL: HanzhongCityType = {
+  ...baseInfo('Hanshui Trail'),
+  position: '(87,91)',
+  tacticalPoints: 5000000,
+  description: "Reduces the garrison's damage taken by 20%",
+  earnings: {
+    woodRate: 360,
+    grainsRate: 360,
+    ironRate: 360,
+  },
+} as const;
+
+const HANZHONG__CITY__HANZHONG_CITY: HanzhongCityType = {
+  ...baseInfo('Hanzhong City'),
+  position: '(75,85)',
+  tacticalPoints: 140000000,
+  description: "Reduces the garrison's damage taken by 20%",
+  earnings: {
+    woodRate: 720,
+    grainsRate: 720,
+    ironRate: 720,
+  },
+} as const;
 
 export const HANZHONG_CITIES: HanzhongCityType[] = [
-  {
-    id: HANZHONG_CITY_IDS.YANGPING_PASS,
-    name: 'Yangping Pass',
-    position: '(61,106)',
-    tacticalPoints: 2000000,
-    description: 'Increases Wei Officers damage to the garrison by 20%',
-    earnings: {
-      woodRate: 180,
-      grainsRate: 180,
-      ironRate: 180,
-    },
-  },
-  {
-    id: HANZHONG_CITY_IDS.NORTH_SUMMIT,
-    name: 'North Summit',
-    position: '(52,74)',
-    tacticalPoints: 2000000,
-    description: 'Increases Shu Officers damage to the garrison by 20%',
-    earnings: {
-      woodRate: 180,
-      grainsRate: 180,
-      ironRate: 180,
-    },
-  },
-  {
-    id: HANZHONG_CITY_IDS.MAMING_PAVILION,
-    name: 'Maming Pavilion',
-    position: '(90,63)',
-    tacticalPoints: 2000000,
-    description: 'Increases Wu Officers damage to the garrison by 20%',
-    earnings: {
-      woodRate: 180,
-      grainsRate: 180,
-      ironRate: 180,
-    },
-  },
-  {
-    id: HANZHONG_CITY_IDS.MIANYANG_COUNTY,
-    name: 'Mianyang County',
-    position: '(99,96)',
-    tacticalPoints: 2000000,
-    description: 'Increases Other Officers damage to the garrison by 20%',
-    earnings: {
-      woodRate: 180,
-      grainsRate: 180,
-      ironRate: 180,
-    },
-  },
-  {
-    id: HANZHONG_CITY_IDS.MOUNT_DINGJUN,
-    name: 'Mount Dingjun',
-    position: '(63,81)',
-    tacticalPoints: 5000000,
-    description: "Reduces the garrison's damage taken by 20%",
-    earnings: {
-      woodRate: 360,
-      grainsRate: 360,
-      ironRate: 360,
-    },
-  },
-  {
-    id: HANZHONG_CITY_IDS.HANSHUI_TRAIL,
-    name: 'Hanshui Trail',
-    position: '(87,91)',
-    tacticalPoints: 5000000,
-    description: "Reduces the garrison's damage taken by 20%",
-    earnings: {
-      woodRate: 360,
-      grainsRate: 360,
-      ironRate: 360,
-    },
-  },
-  {
-    id: HANZHONG_CITY_IDS.HANZHONG_CITY,
-    name: 'Hanzhong City',
-    position: '(75,85)',
-    tacticalPoints: 140000000,
-    description: "Reduces the garrison's damage taken by 20%",
-    earnings: {
-      woodRate: 720,
-      grainsRate: 720,
-      ironRate: 720,
-    },
-  },
-];
+  HANZHONG__CITY__YANGPING_PASS,
+  HANZHONG__CITY__NORTH_SUMMIT,
+  HANZHONG__CITY__MAMING_PAVILION,
+  HANZHONG__CITY__MIANYANG_COUNTY,
+  HANZHONG__CITY__MOUNT_DINGJUN,
+  HANZHONG__CITY__HANSHUI_TRAIL,
+  HANZHONG__CITY__HANZHONG_CITY,
+] as const;
+
+type CacheType = Record<string, HanzhongCityType>;
+
+const __CACHE_BY_ID: CacheType = HANZHONG_CITIES.reduce<CacheType>(
+  (citiesCache, city) =>
+    ({
+      ...citiesCache,
+      [city.id]: city,
+    } as const),
+  {}
+);
+
+const __CACHE_BY_NAME: CacheType = HANZHONG_CITIES.reduce<CacheType>(
+  (citiesCache, city) =>
+    ({
+      ...citiesCache,
+      [city.name]: city,
+    } as const),
+  {}
+);
+
+export type HanzhongCityName = keyof typeof __CACHE_BY_NAME;
+export type HanzhongCityId = keyof typeof __CACHE_BY_ID;
+
+export const getCityByName = (name: HanzhongCityName): HanzhongCityType => __CACHE_BY_NAME[name];
+export const getCityById = (id: HanzhongCityId): HanzhongCityType => __CACHE_BY_ID[id];
+
+export const isCityRequirementSatisfied = (): boolean => {
+  return false;
+};
--- src/app/hanzhong/data/index.ts
+++ src/app/hanzhong/data/index.ts
@@ -1,4 +1,5 @@
-export { HANZHONG_CITIES } from './hanzhong-cities';
+export { HANZHONG_BANDITS } from './hanzhong-bandits';
+export * from './hanzhong-cities';
 export { HANZHONG_DATA } from './hanzhong-data';
 export { HANZHONG_TERRITORIES } from './hanzhong-territories';
 export { HANZHONG_WAR_TIERS } from './hanzhong-war-tiers';
