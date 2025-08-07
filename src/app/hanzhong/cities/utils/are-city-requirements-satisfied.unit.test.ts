f933199272a510c0832fa1c1e3ab799e160979ed
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-07 11:27:35 -0400

    - #92: Fixed test after recursion implemented on cities.

--- src/app/hanzhong/cities/utils/are-city-requirements-satisfied.unit.test.ts
+++ src/app/hanzhong/cities/utils/are-city-requirements-satisfied.unit.test.ts
@@ -8,45 +8,91 @@ import type { RequirementsCache } from '../../requirements/RequirementsCache';
 describe('areCityRequirementsSatisfied()', () => {
   const requirementsCache: RequirementsCache = {};
 
-  const requirement: HanzhongRequirement = {
-    type: 'count',
-    section: 'cities',
-    requirementIds: [cityIdByName('Mount Dingjun'), cityIdByName('Hanshui Trail')],
-    value: 1,
-  };
-
-  it('returns false when no cities are occupied', async () => {
-    const userData: HanzhongUserDataType = {};
-
-    const result = await areCityRequirementsSatisfied(userData, requirement, requirementsCache);
-    expect(result).toBe(false);
-  });
-
-  it('returns false when another city is occupied', async () => {
-    const userData: HanzhongUserDataType = {
-      [cityIdByName('Foo Bar')]: 1,
+  describe('One level check', () => {
+    const requirement: HanzhongRequirement = {
+      type: 'count',
+      section: 'cities',
+      requirementIds: [cityIdByName('Mianyang County'), cityIdByName('Maming Pavilion')],
+      value: 1,
     };
 
-    const result = await areCityRequirementsSatisfied(userData, requirement, requirementsCache);
-    expect(result).toBe(false);
+    it('returns false when no cities are occupied', () => {
+      const userData: HanzhongUserDataType = {};
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(false);
+    });
+
+    it('returns false when another city is occupied', () => {
+      const userData: HanzhongUserDataType = {
+        [cityIdByName('Foo Bar')]: 1,
+      };
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(false);
+    });
+
+    it('returns true when one of the cities is occupied', () => {
+      const userData: HanzhongUserDataType = {
+        [cityIdByName('Mianyang County')]: 1,
+      };
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(true);
+    });
+
+    it('returns true when both of the cities are occupied', () => {
+      const userData: HanzhongUserDataType = {
+        [cityIdByName('Mianyang County')]: 1,
+        [cityIdByName('Mianyang County')]: 1,
+      };
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(true);
+    });
   });
 
-  it('returns true when one of the cities is occupied', async () => {
-    const userData: HanzhongUserDataType = {
-      [cityIdByName('Mount Dingjun')]: 1,
+  describe('With recursion', () => {
+    const requirement: HanzhongRequirement = {
+      type: 'count',
+      section: 'cities',
+      requirementIds: [cityIdByName('Mount Dingjun'), cityIdByName('Hanshui Trail')],
+      value: 1,
     };
 
-    const result = await areCityRequirementsSatisfied(userData, requirement, requirementsCache);
-    expect(result).toBe(true);
-  });
+    it('returns false when no cities are occupied', () => {
+      const userData: HanzhongUserDataType = {};
 
-  it('returns true when both of the cities are occupied', async () => {
-    const userData: HanzhongUserDataType = {
-      [cityIdByName('Mount Dingjun')]: 1,
-      [cityIdByName('Hanshui Trail')]: 1,
-    };
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(false);
+    });
 
-    const result = await areCityRequirementsSatisfied(userData, requirement, requirementsCache);
-    expect(result).toBe(true);
+    it('returns false when another city is occupied', () => {
+      const userData: HanzhongUserDataType = {
+        [cityIdByName('Foo Bar')]: 1,
+      };
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(false);
+    });
+
+    it('returns false when one city is occupied, but not that city requirements', () => {
+      const userData: HanzhongUserDataType = {
+        [cityIdByName('Mount Dingjun')]: 1,
+      };
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(false);
+    });
+
+    it('returns true when recursive are met', () => {
+      const userData: HanzhongUserDataType = {
+        [cityIdByName('Mount Dingjun')]: 1,
+        [cityIdByName('Mianyang County')]: 1,
+      };
+
+      const result = areCityRequirementsSatisfied(userData, requirement, { ...requirementsCache });
+      expect(result).toBe(true);
+    });
   });
 });
