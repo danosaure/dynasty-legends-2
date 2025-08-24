fcfc6d94c5e8ed72a8aff7ca026a831e4f89c7e9
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-24 16:28:31 -0400

    - #97: Fixed imports for HanzhongBanditLevelType

--- src/app/hanzhong/types/hanzhong-bandit-level-type.d.ts
+++ src/app/hanzhong/types/hanzhong-bandit-level-type.d.ts
@@ -1,3 +1,6 @@
+import type { PotentiallyUnknownNumberType } from '../../types';
+import type { HanzhongBanditLevelRewardsType } from './hanzhong-bandit-level-rewards-type';
+
 export type HanzhongBanditLevelType = {
   tacticalPoints: PotentiallyUnknownNumberType;
   rewards: HanzhongBanditLevelRewardsType;
