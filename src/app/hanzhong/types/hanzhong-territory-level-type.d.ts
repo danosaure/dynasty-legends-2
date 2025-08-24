be8fc86591bd4e5844a8a47bc53d6bb391580fff
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-24 16:57:51 -0400

    - #97: Updating type HanzhongTerritoryLevelType

--- src/app/hanzhong/types/hanzhong-territory-level-type.d.ts
+++ src/app/hanzhong/types/hanzhong-territory-level-type.d.ts
@@ -1,15 +1,8 @@
 import type { BaseIDType } from '../../types';
 
 import type { HanzhongTerritoryResourceType } from './hanzhong-territory-resource-type';
-import type { PotentiallyUnknownNumberType } from './potentially-unknown-number-type';
 
 export type HanzhongTerritoryLevelType = BaseIDType & {
-  tacticalPoints: PotentiallyUnknownNumberType;
+  tacticalPoints: number;
   earnings: HanzhongTerritoryResourceType[];
-  team: {
-    green: PotentiallyUnknownNumberType;
-    orange: [PotentiallyUnknownNumberType, PotentiallyUnknownNumberType];
-    red: PotentiallyUnknownNumberType;
-    factionBonus: PotentiallyUnknownNumberType;
-  };
 };
