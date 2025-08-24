cb7117458996374819e30fbb99000691668a4cf0
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-24 16:54:43 -0400

    - #97: Fixing import for type HanzhongTechType

--- src/app/hanzhong/types/hanzhong-tech-type.d.ts
+++ src/app/hanzhong/types/hanzhong-tech-type.d.ts
@@ -1,4 +1,4 @@
-import type { BaseIDType } from './base-id-type';
+import type { BaseIDType } from '../../types';
 import type { HanzhongTechLevelType } from './hanzhong-tech-level-type';
 
 export type HanzhongTechType = BaseIDType & {
