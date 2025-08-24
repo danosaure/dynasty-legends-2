0fc1fb0f59f456bea547a52746d726a5d043a59d
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-24 18:19:02 -0400

    - #63: Removed unused import

--- src/app/hanzhong/types/hanzhong-bandit-attack-type.d.ts
+++ src/app/hanzhong/types/hanzhong-bandit-attack-type.d.ts
@@ -1,4 +1,4 @@
-import type { BaseIDType, PotentiallyUnknownNumberType } from '../../types';
+import type { BaseIDType } from '../../types';
 
 export type HanzhongBanditAttackType = BaseIDType & {
   day: 1 | 2 | 3 | 4 | 5 | 6 | 7;
