ff0b2b67e4907c4b86d28cdaaae13431ce09faf2
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-08-28 16:07:21 -0400

    - Refactored cache data to not use war-tiers data for techs.

--- src/app/hanzhong/data/cache.ts
+++ src/app/hanzhong/data/cache.ts
@@ -1,16 +1,13 @@
 import { HANZHONG_DATA } from './hanzhong-data';
-import type { HanzhongBanditAttackType, HanzhongTechType, HanzhongTerritoryLevelType, HanzhongWarTierType } from '../types';
+import type { HanzhongBanditAttackType, HanzhongTerritoryLevelType } from '../types';
 import type { BaseIDType } from '../../types';
 import { HANZHONG_TERRITORIES } from './hanzhong-territories';
-import { HANZHONG_WAR_TIERS } from '../war-tiers';
 
 type CacheBanditsType = Record<string, HanzhongBanditAttackType>;
-type CacheTechsType = Record<string, HanzhongTechType>;
 type CacheTerritoriesType = Record<string, HanzhongTerritoryLevelType>;
 
 type CacheType = {
   bandits: CacheBanditsType;
-  techs: CacheTechsType;
   territories: CacheTerritoriesType;
 };
 
@@ -24,19 +21,6 @@ const CACHE: CacheType = {
     {} as CacheBanditsType
   ),
 
-  techs: HANZHONG_WAR_TIERS.reduce<CacheTechsType>(
-    (warTiersTechs, warTier: HanzhongWarTierType) =>
-      warTier.techs.reduce<CacheTechsType>(
-        (techs: CacheTechsType, tech: HanzhongTechType) =>
-          ({
-            ...techs,
-            [tech.id]: tech,
-          } as const),
-        warTiersTechs
-      ),
-    {} as CacheTechsType
-  ),
-
   territories: HANZHONG_TERRITORIES.levels.reduce<CacheTerritoriesType>(
     (territories: CacheTerritoriesType, level: HanzhongTerritoryLevelType) =>
       ({
@@ -47,12 +31,7 @@ const CACHE: CacheType = {
   ),
 };
 
-const getKeysFromObject = (obj: Record<string, BaseIDType>): string[] => Object.keys(obj);
 const getObjectById = <T>(obj: Record<string, BaseIDType>, id: string): T | null => (obj[id] as T) ?? null;
 
 export const getBanditById = (id: string): HanzhongBanditAttackType | null =>
   getObjectById<HanzhongBanditAttackType>(CACHE.bandits, id);
-export const getBanditKeys = (): string[] => getKeysFromObject(CACHE.bandits);
-
-export const getTechById = (id: string): HanzhongTechType | null => getObjectById<HanzhongTechType>(CACHE.techs, id);
-export const getTechKeys = (): string[] => getKeysFromObject(CACHE.techs);
