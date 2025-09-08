4ae8cc162e3b4028cdbf5070211faf46404f45d7
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-09-08 11:34:58 -0400

    - #124: Added city officer, faction bonus, and city tactical points.

--- src/app/hanzhong/cities/City.tsx
+++ src/app/hanzhong/cities/City.tsx
@@ -1,13 +1,16 @@
 import Grid from '@mui/material/Grid';
 
 import { CardWrapper } from '../../shared';
-import type { HanzhongCityType } from './types';
 import { HanzhongContextUserCheckbox } from '../ContextCheckbox';
 import { useHanzhongContext } from '../HanzhongContext';
 import { areRequirementsSatisfied, type HanzhongRequirementResponse } from '../requirements';
 import { useEffect, useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { HANZHONG_REQUIREMENT_RESPONSES } from '../requirements/RequirementResponse';
+import type { HanzhongCityType } from './CityType';
+import { getFactionByName } from '../../data';
+import { assetPath } from '../../utils';
+import Typography from '@mui/material/Typography';
 
 export type HanzhongCityProps = {
   city: HanzhongCityType;
@@ -27,9 +30,16 @@ export const HanzhongCity = ({ city }: HanzhongCityProps) => {
 
   const border = `3px solid ${available.satisfied ? 'transparent' : theme.palette.error.main}`;
 
+  let faction = null;
+  if (city.factionBonus) {
+    faction = getFactionByName(city.factionBonus);
+  }
+
   return (
     <CardWrapper sx={{ border }}>
       <Grid container size={{ xs: 12 }} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <img src={assetPath(city.avatar.path)} alt={city.name} width="40px" />
+        {faction && <img src={assetPath(faction.avatar.path)} alt={faction.name} width="24px" />}
         <Grid size={{ xs: 12 }} sx={{ textAlign: 'center' }}>
           {city.name}
         </Grid>
@@ -37,6 +47,11 @@ export const HanzhongCity = ({ city }: HanzhongCityProps) => {
           <HanzhongContextUserCheckbox id={city.id} />
         </Grid>
       </Grid>
+      {city.tacticalPoints.map((tacticalPoint) => (
+        <Typography key={tacticalPoint} fontSize="11px" align="center">
+          {tacticalPoint}
+        </Typography>
+      ))}
     </CardWrapper>
   );
 };
--- src/app/hanzhong/cities/types/HanzhongCityName.ts
+++
@@ -1,3 +0,0 @@
-import type { __CACHE_BY_NAME } from '../data/cache';
-
-export type HanzhongCityName = keyof typeof __CACHE_BY_NAME;
