13ec6def1ae7c26d833f66783049621baa0ff8e3
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-09-12 17:10:51 -0400

    - #139: Refactored Banquet Quiz to detect completion of all the questions.

--- src/app/banquet-quiz/Questionnaire.tsx
+++ src/app/banquet-quiz/Questionnaire.tsx
@@ -6,18 +6,18 @@ import { PaperWrapper } from '../shared';
 import { BANQUET_QUIZ } from './data';
 import { BanquetQuizQuestion } from './Question';
 import type { BanquetQuizType } from './types';
-import { LeitnerSystem } from '../../libs';
+import Typography from '@mui/material/Typography';
+import { LeitnerSystem, type LeitnerBox, type LeitnerBoxes } from '../../libs/leitner-system';
 
 export const BanquetQuizQuestionnaire = () => {
   const [selected, setSelected] = useState<string>('');
   const [question, setQuestion] = useState<BanquetQuizType>();
   const [leitner, setLeitner] = useState<LeitnerSystem<BanquetQuizType> | null>(null);
   const [questionGenerator, setQuestionGenerator] = useState<Generator<BanquetQuizType | undefined>>();
+  const [isSessionDone, setIsSessionDone] = useState<boolean>(false);
 
-  useEffect(() => {
-    // TODO: fetch data from persistence.
-
-    const questionnaire = new LeitnerSystem<BanquetQuizType>(BANQUET_QUIZ, 0);
+  const initializeSession = (sessionId: number, boxes: LeitnerBoxes, retiredDeck: LeitnerBox) => {
+    const questionnaire = new LeitnerSystem<BanquetQuizType>(BANQUET_QUIZ, sessionId, boxes, retiredDeck);
     setLeitner(questionnaire);
 
     const generator = questionnaire.questions();
@@ -25,6 +25,11 @@ export const BanquetQuizQuestionnaire = () => {
 
     const questionIteration = generator.next();
     setQuestion(questionIteration.value);
+  };
+
+  useEffect(() => {
+    // TODO: fetch data from persistence.
+    initializeSession(0, [[], [], [], [], [], [], [], [], [], []], []);
   }, []);
 
   const onAnswerSelected = (label: string) => {
@@ -33,6 +38,7 @@ export const BanquetQuizQuestionnaire = () => {
       const correct = question.options.find((option) => option.label === label)?.answer;
       leitner?.rebox(question.id, correct ?? false);
     }
+    setIsSessionDone(leitnerStats.questionIndex === leitnerStats.currentDeckSize);
   };
 
   const nextQuestion = () => {
@@ -40,20 +46,26 @@ export const BanquetQuizQuestionnaire = () => {
     if (questionGenerator) {
       const questionIteration = questionGenerator.next();
       if (questionIteration.done) {
-        console.log('DONE QUESTIONNAIRE...');
+        setIsSessionDone(true);
       } else {
         setQuestion(questionIteration.value);
       }
     }
   };
 
-  if (!leitner || !question) {
+  const startNextSession = () => {
+    initializeSession((leitnerStats.sessionId as number) + 1, leitnerStats.boxes, leitnerStats.retiredDeck);
+    setIsSessionDone(false);
+    setSelected('');
+  };
+
+  if (!leitner) {
     return null;
   }
 
   const leitnerStats = leitner.stats();
 
-  console.log('leitnerStats=', leitnerStats);
+  // console.log('leitnerStats=', JSON.stringify(leitnerStats, null, 2));
 
   let percentJSX = null;
   if (leitnerStats.answered) {
@@ -67,16 +79,30 @@ export const BanquetQuizQuestionnaire = () => {
   return (
     <PaperWrapper sx={{ p: 3 }}>
       <Grid container spacing={2} direction="column">
-        <BanquetQuizQuestion question={question} onAnswerSelected={selected ? () => {} : onAnswerSelected} selected={selected} />
+        {question ? (
+          <BanquetQuizQuestion question={question} onAnswerSelected={selected ? () => {} : onAnswerSelected} selected={selected} />
+        ) : (
+          <Typography color="warning">No questions for this session.</Typography>
+        )}
         <Grid container>
-          <Grid size={{ xs: 6 }}>
+          <Grid size={{ xs: 4 }}>Session: {leitnerStats.sessionId + 1}</Grid>
+          <Grid size={{ xs: 4 }}>
             Progress: {leitnerStats.questionIndex ?? 0 + 1}/{leitnerStats.currentDeckSize}
           </Grid>
-          <Grid size={{ xs: 6 }}>{percentJSX}</Grid>
+          <Grid size={{ xs: 4 }}>{percentJSX}</Grid>
         </Grid>
-        <Button onClick={() => nextQuestion()} variant="outlined" disabled={!selected}>
-          Next Question
-        </Button>
+        {isSessionDone || !question ? (
+          <>
+            <Typography color="success">Session completed.</Typography>
+            <Button onClick={() => startNextSession()} variant="contained">
+              Start Next Session
+            </Button>
+          </>
+        ) : (
+          <Button onClick={() => nextQuestion()} variant="outlined" disabled={!selected}>
+            Next Question
+          </Button>
+        )}
       </Grid>
     </PaperWrapper>
   );
