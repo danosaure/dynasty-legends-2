dd0f5e483e8eaa66c94a8b5484d6e38cc44d31bf
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-09-12 12:07:48 -0400

    - #139: Adding `id` to data.

--- src/app/banquet-quiz/types.ts
+++ src/app/banquet-quiz/types.ts
@@ -1,9 +1,11 @@
+import type { BaseIDType } from '../types';
+
 export type BanquetQuizAnswerType = {
   label: string;
   answer?: boolean;
 };
 
-export type BanquetQuizType = {
+export type BanquetQuizType = BaseIDType & {
   question: string;
   options: BanquetQuizAnswerType[];
 };
