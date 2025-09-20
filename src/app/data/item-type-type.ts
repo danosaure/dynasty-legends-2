23171278f1e75dfe64041f2486c6e3fbc36f535e
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-09-20 17:05:05 -0400

    - Gala Treats data

---
+++ src/app/data/item-type-type.ts
@@ -0,0 +1,11 @@
+export type ItemTypeType =
+  | 'Avatar Frame'
+  | 'Consumables'
+  | 'Currency'
+  | 'Image Halo'
+  | 'Materials'
+  | 'Officer'
+  | 'Pack'
+  | 'Soul Bead'
+  | 'Universal Shard'
+  | 'Weapon Shard';
---
+++ src/app/data/reward-type.ts
@@ -0,0 +1,7 @@
+import type { ItemTypeType } from './item-type-type';
+
+export type RewardType = {
+  type: ItemTypeType;
+  name: string;
+  count: number;
+};
