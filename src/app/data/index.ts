be1c1f8333d4ce1d9f24d2ab44b0a9e0adf045d2
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-09-20 17:05:05 -0400

    - Gala Treats data

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -1,6 +1,8 @@
 export * from './aptitudes';
 export * from './factions';
+export type { ItemTypeType } from './item-type-type';
 export * from './mounts';
 export * from './officer-bonds';
 export * from './officer-types';
 export * from './officers';
+export type { RewardType } from './reward-type';
