23f6a7f4b52c4f98be9093137f1a2385acb3d40b
Danosaure <1444063+danosaure@users.noreply.github.com>
2025-09-21 00:25:16 -0400

    - Adding Apex Showdown shop data.

---
+++ src/app/data/packs/exotic-halo-pyo-box.ts
@@ -0,0 +1,26 @@
+import { convertNamesToRewards } from '../convert-names-to-rewards';
+import type { PackType } from './PackType';
+
+export const EXOTIC_HALO_PYO_BOX: PackType = {
+  type: 'Pack',
+  name: 'Exotic Halo PYO Box',
+  items: convertNamesToRewards('Image Halo', [
+    'Nether Soulfire',
+    'Dragonfly Waltz',
+    "Lightning's Might",
+    'Azure Waves',
+    'Snowflakes',
+    'Phantom Fire',
+    'Affection',
+    'Innocence',
+    'Fireworks',
+    'Melody of Joy',
+    'Free Notes',
+    'Crimson Lightning',
+    'Snow Bells',
+    'Star Spectrum',
+    'Rosy Shade',
+    'Brilliant Glow',
+    'Star Guide',
+  ]),
+};
